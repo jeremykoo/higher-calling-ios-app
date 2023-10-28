720cf9a4e9cf40a816a60f23d1c97325c0f3839f
Jeremy Koo <skykoo2000@gmail.com>
2023-10-28 16:26:02 -0700

    Initial Commit

---
+++ HigherCalling/HigherCallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherCallingApp.swift
+//  HigherCalling
+//
+//  Created by Jeremy Koo on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HigherCallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
